d924e1c15fd436fdee651583022da4e10e87a79a
GarvitKaushikCrackle <gkaushik@crackle.tech>
2025-08-03 09:47:01 +0530

    swipe back

--- stunner/stunnerApp.swift
+++ stunner/stunnerApp.swift
@@ -20,3 +20,24 @@ struct stunnerApp: App {
     }
 }
 
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+    super.viewDidLoad()
+    interactivePopGestureRecognizer?.delegate = self
+    }
+
+    // Allows swipe back gesture after hiding standard navigation bar with .navigationBarHidden(true).
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+    viewControllers.count > 1
+    }
+
+    // Allows interactivePopGestureRecognizer to work simultaneously with other gestures.
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+    viewControllers.count > 1
+    }
+
+    // Blocks other gestures when interactivePopGestureRecognizer begins (my TabView scrolled together with screen swiping back)
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+    viewControllers.count > 1
+    }
+}
